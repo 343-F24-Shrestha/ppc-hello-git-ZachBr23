3e19a4f46f4789ba052e93bc313f753198871cfa
Zach Breitbeil <breitbzt@dukes.jmu.edu>
2026-03-12 23:06:25 -0400

    updated .jpg and updated button with email

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let contact = document.getElementById("contact");
+let email = document.getElementById("email");
 
 contact.addEventListener("click", sayHello);
 
 function sayHello() {
-  alert("Under Construction - Thanks for visiting my profile!");
+  if (email.textContent === "") {
+    email.textContent = "breitbzt@dukes.jmu.edu";
+    email.style.backgroundColor = "yellow";
+    contact.textContent = "Hide!";
+  } else {
+    email.textContent = "";
+    email.style.backgroundColor = "";
+    contact.textContent = "Say Hello!";
+  }
 }
